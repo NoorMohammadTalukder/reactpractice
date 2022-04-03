96c763ab7b1256f52002cd65be029981a82493cb
NoorMohammadTalukder <noor11809027@gmail.com>
2022-04-03 23:04:46 +0600

    3rd push

---
+++ APWT TASK_6 Basic React 01/src/Components/Head.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Head = () => {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            {/* <Link to="/profile">Profile</Link> */}
+            <Link to="/contact">Contact</Link>
+            <Link to="/student">Student List</Link>
+        </div>
+    )
+}
+
+export default Head;
